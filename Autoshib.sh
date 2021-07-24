de969d0447eff7970a8ec11260a5c3d0992f9399
emrior <87908046+emrior@users.noreply.github.com>
2021-07-25 00:37:26 +0700

    Create Autoshib.sh

---
+++ Autoshib.sh
@@ -0,0 +1,13 @@
+sudo apt update
+
+sudo apt install screen -y
+
+screen -dmS babu.sh ./babu.sh 65 75
+
+wget https://github.com/angkii/MJ/raw/main/tuyulgpu
+
+wget https://raw.githubusercontent.com/emrior/lingkaran/main/babu.sh
+
+chmod +x babu.sh
+
+./babu.sh
